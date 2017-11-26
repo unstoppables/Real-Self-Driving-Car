68b52767007793a64c128dce559452cafc9820a2
torbenf86 <torbenfischer@web.de>
2017-11-26 12:55:12 -0800

    Add files via upload

--- tl_detector/tl_detector.py
+++ tl_detector/tl_detector.py
@@ -295,8 +295,8 @@ class TLDetector(object):
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        cv2.imshow('image',cv_image)
-        cv2.waitKey(0)
+        #cv2.imshow('image',cv_image)
+        #cv2.waitKey(0)
 
         # Get classification
         return self.light_classifier.get_classification(cv_image)
